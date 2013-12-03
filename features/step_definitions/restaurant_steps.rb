9f8c13469f51cdecffcd9baedc579c67b3ff789c
fori7772 <forrest.ridler@colorado.edu>
2013-12-03 16:42:14 -0700

    fixed html issue in restaurant index view

--- features/step_definitions/restaurant_steps.rb
+++ features/step_definitions/restaurant_steps.rb
@@ -1,37 +1,38 @@
 Given(/^the following restaurants exist:$/) do |table|
-  table.hashes.each do |restaurant|
-    Restaurant.create(restaurant)
-  end
+	table.hashes.each do |restaurant|
+		Restaurant.create(restaurant)
+	end
 end
 
 When(/^(?:|I )go to (.+)$/) do |page_name|
-  visit(restaurants_path)
+	visit(restaurants_path)
 end
 
 Given(/^I (un)?check the following cuisines: (.*?)$/) do |uncheck, cuisines|
-  cuisines.split(/,\s*/).each do |cuisine|
-    if uncheck.nil? then check("cuisines_#{cuisine}")
-    else uncheck("cuisines_#{cuisine}")
-    end
-  end
+	cuisines.split(/,\s*/).each do |cuisine|
+		if uncheck.nil? then check("cuisines_#{cuisine}")
+		else uncheck("cuisines_#{cuisine}")
+		end
+	end
 end
 
 When(/I press (.*?)/) do |arg1|
-  click_button('cuisines_submit')
+	# print page.html
+	click_button('cuisines_submit')
 end
 
 Then(/^I should see "(.*?)"$/) do |arg1|
-  page.find('td', :text => arg1)
+	page.find('td', :text => arg1)
 end
 
 
 Then(/^I should not see "(.*?)"$/) do |arg1|
-  expect { page.find('td', :text => arg1) }.to raise_error(Capybara::ElementNotFound)
+	expect { page.find('td', :text => arg1) }.to raise_error(Capybara::ElementNotFound)
 end
 
 # Unchecking all of the restaurants is not allowed
 Then(/I should see (all|some) of the restaurants/) do |arg|
-  Restaurant.all.each do |restaurant|
-    page.find('td', :text => restaurant.name)
-  end
+	Restaurant.all.each do |restaurant|
+		page.find('td', :text => restaurant.name)
+	end
 end
